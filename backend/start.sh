8d01363aa217525be3d4d0a07162677778c73fe3
sumitmahankale <sumitmahankale7777@gmail.com>
2025-08-15 10:37:58 +0530

    Update start script with Java runtime setup

--- backend/start.sh
+++ backend/start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+echo "Setting up Java runtime..."
+source ~/.sdkman/bin/sdkman-init.sh
+sdk use java 11.0.12-tem
+
+echo "Starting Spring Boot application..."
 java -Dserver.port=$PORT -Dspring.profiles.active=prod -jar target/FinJar-0.0.1-SNAPSHOT.jar
