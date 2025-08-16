d161abe7be74a552f3fff2c2ab4b62b07f579abf
sumitmahankale <sumitmahankale7777@gmail.com>
2025-08-16 17:47:29 +0530

    Fix JSON syntax error in package.json - remove invalid comments

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 import './index.css'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-// Force rebuild - Sat, Aug 16, 2025  5:42:30 PM
