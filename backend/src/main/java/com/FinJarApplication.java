aec4f88b155f2d6411953884795fddf6d4930dde
sumitmahankale <sumitmahankale7777@gmail.com>
2025-08-16 09:36:37 +0530

    Fix duplicate package declarations in FinJarApplication.java

--- backend/src/main/java/com/FinJarApplication.java
+++ backend/src/main/java/com/FinJarApplication.java
@@ -3,11 +3,6 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-package com;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class FinJarApplication {
 
